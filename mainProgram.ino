c26b949b9233109e11abb0f6ccb4eeba3206fda1
M Fathoni Nurrohman <muh.fathoni77@yahoo.co.id>
2016-10-10 09:17:28 +0700

    Add files via upload

--- mainProgram.ino
+++ mainProgram.ino
@@ -1,25 +1,25 @@
-///////////////////////////////////////////////////////////////////////////////////////////
-//          Code line follower yang memiliki beberapa case dalam rintangannya            //
-//                                                                                       //
-//  case 1 : default                                                                     //
-//   case ini digunakan untuk minimal menyelesaikan sampai check point 1 pada lintasan   //
-//  case 2 : indikator 1                                                                 //
-//    case ini digunakan untuk minimal menyelesaikan sampai check point 2 pada lintasan  //
-//  case 3 : indikator 2                                                                 //
-//    case ini digunakan untuk minimal menyelesaikan sampai check point 3 pada lintasan  //
-//  case 4 : indikator 3                                                                 //
-//    case ini digunakan untuk minimal menyelesaikan sampai check point 4 pada lintasan  //
-//                                                                                       //
-//  dibuat oleh ://                                                                      //
-//    Calmantara Sumpono Putra - 13215032                                                // 
-//    Muh Fathoni Nurrahman - 13215040                                                   //
-///////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////
+//           Code line follower yang memiliki beberapa case dalam rintangannya             //
+//                                                                                         //
+//   case 1 : default                                                                      //
+//    case ini digunakan untuk minimal menyelesaikan sampai check point 1 pada lintasan    //
+//   case 2 : indikator 1                                                                  //
+//     case ini digunakan untuk minimal menyelesaikan sampai check point 2 pada lintasan   //
+//   case 3 : indikator 2                                                                  //
+//     case ini digunakan untuk minimal menyelesaikan sampai check point 3 pada lintasan   //
+//   case 4 : indikator 3                                                                  //
+//     case ini digunakan untuk minimal menyelesaikan sampai check point 4 pada lintasan   //
+//                                                                                         //
+//  dibuat oleh ://                                                                        //
+//    Calmantara Sumpono Putra - 13215032                                                  // 
+//    Muh Fathoni Nurrahman - 13215040                                                     //
+/////////////////////////////////////////////////////////////////////////////////////////////
 #include <SoftwareSerial.h>
 #include <AFMotor.h>
 
-///////////////////////////////////////////////////////////////////////////////////////////
-//                        Initiation for Constanta and Variable                          //
-///////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////
+//                         Initiation for Constanta and Variable                           //
+/////////////////////////////////////////////////////////////////////////////////////////////
 //terminal shield motor
 AF_DCMotor motor_l(1);
 AF_DCMotor motor_r(4);
@@ -42,10 +42,13 @@ int cal_black ; //calibrating black line
 int button_Ind = 0; //inisiasi untuk push button
 float speed_l = 0;
 float speed_r = 0;
+int bacaBagian = 0; // metode pembacaan sensor;
+int count = 0; //untuk case yg perlu counter
+int counter; // sensor mana yg di pake untuk counter A8 = Kiri || A13 = Kanan
 
-///////////////////////////////////////////////////////////////////////////////////////////
-//                            Deklarasi Procedure Function                               //
-///////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////
+//                             Deklarasi Procedure Function                                //
+/////////////////////////////////////////////////////////////////////////////////////////////
 void bacaSensor();
 //  untuk input nilai sens[i]
 void olahSensor(); 
@@ -57,6 +60,7 @@ void default_program();
 void mode_1();
 void mode_2();
 void mode_3();
+void PID();
 //  Mode yang digunakan dalam lomba
 void motor_speed_default(int f);
 //  pengatur gerak motor (PWM) dari PID
@@ -89,7 +93,7 @@ void setup ()
   }
   
   //inisiasi sensor
-  bacaSensor();	
+  bacaSensor(0);	
 }
 
 void loop () 
@@ -105,20 +109,42 @@ void loop ()
 ///////////////////////////////////////////////////////////////////////////////////////////
 //                            Algoritma Procedure Function                               //
 ///////////////////////////////////////////////////////////////////////////////////////////
-void bacaSensor()
+void bacaSensor(int bagian)
 {
+  //all sensor is active
+  if (bagian == 0)
+  {
+    sens[0] = analogRead(A8);
+    sens[1] = analogRead(A9);
+    sens[2] = analogRead(A10);
+    sens[3] = analogRead(A11);
+    sens[4] = analogRead(A12);
+    sens[5] = analogRead(A13);
+  }
+  //left sensor is active
+  if (bagian == 2){
+    sens[0] = analogRead(A8);
+    sens[1] = analogRead(A9);
+    sens[2] = analogRead(A10);
+    sens[3] = analogRead(A11);
+    sens[4] = 0;
+    sens[5] = 0;
+  }
+  //right sensor is active
+  if (bagian == 1){
+    sens[0] = 0;
+    sens[1] = 0;
+    sens[2] = analogRead(A10);
+    sens[3] = analogRead(A11);
+    sens[4] = analogRead(A12);
+    sens[5] = analogRead(A13);
+  }
   // untuk pengisian/pembacaan nilai sens[i]
-  sens[0] = analogRead(A8);
-  sens[1] = analogRead(A9);
-  sens[2] = analogRead(A10);
-  sens[3] = analogRead(A11);
-  sens[4] = analogRead(A12);
-  sens[5] = analogRead(A13);
 }
 
-void olahSensor()
+void olahSensor(int bagian)
 {
-  bacaSensor();
+  bacaSensor(bagian);
   for(int i = 0; i < 6; i++)
   {
     if(sens[i] < cal_black) 
@@ -189,7 +215,45 @@ void olahSensor()
 }
 
 void default_program(){
-  
+  // pengaktifan mode saat menyentuh lintasan T
+  olahSensor(bacaBagian);
+  if ( (sens[0] == 0) && (sens[5] == 0) )
+  {
+    if ( (sens[0] == 1) && (sens[5] == 1) )
+    {
+      count = 0;
+      bacaBagian = 1;
+      counter = A13;    
+      /*motor_l.run(FORWARD);
+      motor_l.setSpeed(220);
+      motor_r.run(FORWARD);
+      motor_r.setSpeed(220);
+      delay(100);*/
+    }
+  }
+  if (analogRead(counter) == 0)
+  {
+    if (analogRead(counter) == 1)
+    {
+      count++;
+    }
+  }
+  switch (count) 
+  {
+    case 1 :
+      bacaBagian = 2;
+      counter = A8;
+      
+    break;
+    case 2 : 
+      bacaBagian = 1;
+      counter = A13;
+    break;  
+    case 3 :
+      bacaBagian = 0;
+      count = 0;
+    break;
+  }
 }
 //Prosedur untuk indikator case
 void indicator_in()
@@ -209,7 +273,7 @@ void indicator_in()
     ind_Value = 0;
   }
 
-  //Calibrating for black line 
+  //default 
   if (ind_Value == 0)
   {
     digitalWrite(ind[0], LOW);
@@ -254,7 +318,7 @@ void calculate_PID()
   PID_value = (Kp * P) + (Ki * I) + (Kd * D);
   previous_error = error;
 }
-
+//procedure for motor speed
 void motor_speed_default(int f)
 {
   //inisiasi nilai PID
